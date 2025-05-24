bc88a35f3424a042954296c0b86988fb16d9c37b
KEVAL CHHEDA <kvc@somaiya.edu>
2025-05-24 18:51:28 +0530

    Lowest Common Ancestor
    Lowest Common Ancestor

---
+++ src/com/programmingKC/dsa/leetcode75/BinaryTree/LowestCommonAncestor.java
@@ -0,0 +1,52 @@
+package com.programmingKC.dsa.leetcode75.BinaryTree;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int val) { this.val = val; }
+}
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q)
+            return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null)
+            return root;
+
+        return left != null ? left : right;
+    }
+}
+
+public class LowestCommonAncestor {
+    public static void main(String[] args) {
+        // Constructing the binary tree
+        //         3
+        //       /   \
+        //      5     1
+        //     / \   / \
+        //    6   2 0   8
+        //       / \
+        //      7   4
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+
+        TreeNode p = root.left;             // Node 5
+        TreeNode q = root.left.right.right; // Node 4
+
+        Solution sol = new Solution();
+        TreeNode lca = sol.lowestCommonAncestor(root, p, q);
+
+        System.out.println("Lowest Common Ancestor of " + p.val + " and " + q.val + " is: " + lca.val);
+    }
+}

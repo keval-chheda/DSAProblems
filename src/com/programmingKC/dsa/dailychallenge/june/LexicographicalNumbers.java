a67d99f1ae944f87791625431a01b9371547c5fa
KEVAL CHHEDA <kvc@somaiya.edu>
2025-06-08 19:00:07 +0530

    Lexicographical Numbers June Daily Challenge
    Lexicographical Numbers June Daily Challenge

---
+++ src/com/programmingKC/dsa/dailychallenge/june/LexicographicalNumbers.java
@@ -0,0 +1,21 @@
+package com.programmingKC.dsa.dailychallenge.june;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class LexicographicalNumbers {
+    static public List<Integer> lexicalOrder(int n) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            ans.add(i);
+        }
+
+        ans.sort((a, b) -> {
+            String s1 = String.valueOf(a);
+            String s2 = String.valueOf(b);
+            return s1.compareTo(s2);
+        });
+
+        return ans;
+    }
+}
